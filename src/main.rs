ca8a95e0c1421838d4588d1eef122155fa81e30c
Alex Servirog <servalex1@gmail.com>
2025-07-30 02:45:21 +0200

    raylib initialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use raylib::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    const WIDTH: i32 = 800;
+    const HEIGHT: i32 = 600;
+
+    let (mut rl, thread) = raylib::init()
+        .size(WIDTH, HEIGHT)
+        .title("flow field rust")
+        .build();
+
+    rl.set_target_fps(60);
+
+    while !rl.window_should_close() {
+        let mut drawing_context = rl.begin_drawing(&thread);
+
+        drawing_context.clear_background(Color::BLACK);
+    }
 }
